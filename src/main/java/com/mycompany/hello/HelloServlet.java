00234611c69a917ac078aa7caedb2abea3821b39
sanjusanjay0011 <78024048+sanjusanjay0011@users.noreply.github.com>
2024-05-01 17:55:12 +0530

    Code updated

--- src/main/java/com/mycompany/hello/HelloServlet.java
+++ src/main/java/com/mycompany/hello/HelloServlet.java
@@ -1,22 +1,23 @@
-package com.mycompany.hello;
-
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-public class HelloServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response) throws IOException {
-        PrintWriter out = response.getWriter();
-        out.println("<HTML>");
-        out.println("<HEAD>");
-        out.println("<TITLE>Hello Servlet</TITLE>");
-        out.println("</HEAD>");
-        out.println("<BODY>");
-        out.println("Hello Servlet");
-        out.println("</BODY>");
-        out.println("</HTML>");
+public class LandingPageServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        // Output HTML content
+        response.getWriter().println("<!DOCTYPE html>");
+        response.getWriter().println("<html>");
+        response.getWriter().println("<head>");
+        response.getWriter().println("<title>Welcome to Our Website</title>");
+        response.getWriter().println("</head>");
+        response.getWriter().println("<body>");
+        response.getWriter().println("<h1>Welcome to Our Website</h1>");
+        response.getWriter().println("<p>This is a simple landing page.</p>");
+        response.getWriter().println("</body>");
+        response.getWriter().println("</html>");
     }
 }
